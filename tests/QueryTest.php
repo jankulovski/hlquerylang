4842d072e0e96afeeefb38c52e9c3f03444b2d6c
Kiko <ejhiko@gmail.com>
2016-01-15 12:20:32 +0100

    Unit test conf

---
+++ tests/QueryTest.php
@@ -0,0 +1,15 @@
+<?hh
+
+namespace hlql;
+
+use hlql\Query\Query;
+
+class QueryTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_valid_query(): bool {
+
+        // ...
+        return true;
+    }
+
+}
